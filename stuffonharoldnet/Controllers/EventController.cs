f69ea81220a64ae943213bdf363b04dd2bb38f8b
Jason Dees <jhdees@gmail.com>
2018-10-28 09:31:44 -0400

    Adding an event controller and stubs

---
+++ stuffonharoldnet/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace stuffonharoldnet.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+    public class EventController
+    {
+
+		[HttpPost(Name = "SwipedLeft")]
+		public async Task<ActionResult> SwipedLeft() {
+			return null;
+		}
+
+		[HttpPost(Name = "SwipedRight")]
+		public async Task<ActionResult> SwipedRight() {
+			return null;
+		}
+
+		[HttpPost(Name = "HitNext")]
+		public async Task<ActionResult> HitNext() {
+			return null;
+		}
+    }
+}
