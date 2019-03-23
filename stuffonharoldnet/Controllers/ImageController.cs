e39a922757de3f846b82d8af65d43d86862c8601
Jason Dees <jhdees@gmail.com>
2019-03-23 13:26:52 -0400

    Adding support for image index

--- stuffonharoldnet/Controllers/ImageController.cs
+++ stuffonharoldnet/Controllers/ImageController.cs
@@ -24,34 +24,53 @@ namespace jhdeescomnet.Controllers {
 			return await GetImageFileList();
 		}
 
-		[HttpGet("random", Name = "GetImage")]
-		public async Task<IActionResult> GetImage([FromQuery]int width = 0, [FromQuery]int height = 0) {
+		[HttpGet("random", Name = "GetRandomImage")]
+		public async Task<IActionResult> GetRandomImage([FromQuery]int width = 0, [FromQuery]int height = 0) {
 
 			var images = await GetImageFileList();
+			var index = new Random().Next(0, images.Count());
 			var imageBytes = 
-				await _azureImageService.GetImage(images.ElementAt(new Random().Next(0, images.Count())));
+				await _azureImageService.GetImage(images.ElementAt(index));
 
+			return File(imageBytes.Resize(width, height), "image/jpeg");
+		}
+
+		[HttpGet("{index}", Name = "GetImage")]
+		public async Task<IActionResult> GetSpecificImage(int index, [FromQuery]int width = 0, [FromQuery]int height = 0) {
+
+			var images = await GetImageFileList();
+			if (index > images.Count) {
+				return BadRequest($"There is no image at index {index}");
+			}
+
+			var imageBytes = await _azureImageService.GetImage(images.ElementAt(index));
+
+			return File(imageBytes.Resize(width, height), "image/jpeg");
+		}
+
+		async Task<List<string>> GetImageFileList() {
+			return await _azureImageService.GetImageNames();
+		}
+
+	}
+
+	internal static class Extensions {
+
+		internal static byte[] Resize(this byte[] imageBytes, int width, int height) {
 			using (var image = new MagickImage(imageBytes)) {
-                width = width == 0 ? image.Width : width;
-                height = height == 0 ? image.Height : height;
+				width = width == 0 ? image.Width : width;
+				height = height == 0 ? image.Height : height;
 
 				var ratio = (double)image.Width / image.Height;
 				var scalePercentage = new Percentage(100);
-				 
+
 				scalePercentage = width / height > ratio
 					? new Percentage((double)height / image.Height * 100)
 					: new Percentage((double)width / image.Width * 100);
 
 				image.Resize(scalePercentage);
-
-				return File(image.ToByteArray(), "image/jpeg");
+				return image.ToByteArray();
 			}
-
-			throw new Exception();
-		}
-
-		async Task<List<string>> GetImageFileList() {
-			return await _azureImageService.GetImageNames();
 		}
 	}
 }
